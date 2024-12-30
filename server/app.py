b8751eb83bb21a512916fa84f61da31f36ebad13
Phaedra Sanon <71579580+ph4iry@users.noreply.github.com>
2024-12-30 03:13:18 -0500

    reduced redundancy

--- server/app.py
+++ server/app.py
@@ -10,10 +10,10 @@ from haversine import haversine
 app = Flask(__name__)
 CORS(app)
 
+dat = rasterio.open(r"./map.tif")
+z = dat.read()[0]
+
 def map_location_to_pixel(lat, lon):
-  dat = rasterio.open(r"./map.tif")
-  z = dat.read()[0]
-  
   idx = dat.index(lon, lat, precision=1E-6)
   return z[idx]
     
@@ -37,35 +37,33 @@ def classify_sqm(rgb): # sqm = sky quality meter, in mag/arcsec^2
   return None
 
 def find_nearest_dark_location(lat, lon):
-  with rasterio.open(r"./map.tif") as dat:
-    z = dat.read()[0]
-    height, width = z.shape
-    
-    center_idx = dat.index(lon, lat, precision=1E-6)
-    center_r, center_c = center_idx
-    max_distance = max(height, width)
-    
-    for radius in range(1, max_distance):
-      # sweep around the center px at radius r
-      for angle in range(0, 360, 5):
-        angle_rad = math.radians(angle)
-        d_row = int(radius * math.sin(angle_rad))
-        d_col = int(radius * math.cos(angle_rad))
-        cur_row = center_r + d_row
-        cur_col = center_c + d_col
-        
-        
-        if 0 <= cur_row < height and 0 <= cur_col < width:
+  height, width = z.shape
+  
+  center_idx = dat.index(lon, lat, precision=1E-6)
+  center_r, center_c = center_idx
+  max_distance = max(height, width)
+  
+  for radius in range(1, max_distance):
+    # sweep around the center px at radius r
+    for angle in range(0, 360, 5):
+      angle_rad = math.radians(angle)
+      d_row = int(radius * math.sin(angle_rad))
+      d_col = int(radius * math.cos(angle_rad))
+      cur_row = center_r + d_row
+      cur_col = center_c + d_col
+      
+      
+      if 0 <= cur_row < height and 0 <= cur_col < width:
+        nearest_lon, nearest_lat = dat.xy(cur_row, cur_col)
+        if not globe.is_land(nearest_lat, nearest_lon):
+          continue
+        rgb = np.array([z[cur_row, cur_col]])
+        bortle = classify_sqm(rgb)
+        if bortle is not None and int(bortle.split("-")[0]) <= 4:
           nearest_lon, nearest_lat = dat.xy(cur_row, cur_col)
-          if not globe.is_land(nearest_lat, nearest_lon):
-            continue
-          rgb = np.array([z[cur_row, cur_col]])
-          bortle = classify_sqm(rgb)
-          if bortle is not None and int(bortle.split("-")[0]) <= 4:
-            nearest_lon, nearest_lat = dat.xy(cur_row, cur_col)
-            return nearest_lat, nearest_lon, radius, bortle
-    
-    return None
+          return nearest_lat, nearest_lon, radius, bortle
+  
+  return None
 
 @app.route("/")
 @cross_origin()
